59c69d11085314a114581c464b6f10328417a807
pedrokaut <lexus04111@gmail.com>
2025-09-05 15:56:07 -0300

    add primeiros codigos!

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) throws java.io.IOException {
-        String[] nomes = new String[10];
-        int[] idades = new int[10];
+    /*
+     *
+     * Para cada questão, crie uma classe, como a que está exposta a seguir.
+     * Padrão: questao1(), questao()2 ... questao10(), por exemplo.
+     *
+     * */
+    public static void questao1() {
+        Scanner input = new Scanner(System.in);
+        float etqmedio, qmin, qmax;
+        System.out.println("Digite a quantidade minima");
+        qmin = input.nextFloat();
 
-        for (int i = 0; i < 10; i++) {
-            System.out.print("Digite o nome da pessoa " + (i + 1) + ": ");
-            nomes[i] = lerLinha();
-
-            System.out.print("Digite a idade de " + nomes[i] + ": ");
-            idades[i] = Integer.parseInt(lerLinha());
-        }
-
-        System.out.println("\nLista de Dados");
-        for (int i = 0; i < 10; i++) {
-            System.out.println((i + 1) + " - " + nomes[i] + " tem " + idades[i] + " anos.");
-        }
+        System.out.println("Digite a quantidade maxima");
+        qmax = input.nextFloat();
+        etqmedio = (qmax - qmin) / 2;
+        System.out.println("o estoque medio e" + etqmedio);
+    }
+    public static void questao2() {
+        Scanner input = new Scanner(System.in);
+        float cotacao,valordola,valorreal,resultado;
+        System.out.println("Qual a cotação do dolar?");
+        cotacao = input.nextFloat();
+        System.out.println("Qual a valor do dolar?");
+        valordola = input.nextFloat();
+        System.out.println("Qual a valor de reais que deseja");
+        valorreal = input.nextFloat();
+        resultado = (valordola * valorreal);
+        System.out.println("o resultado da conversão e"+resultado);
     }
 
-
-    public static String lerLinha() throws java.io.IOException {
-        StringBuilder sb = new StringBuilder();
-        int c;
-        while ((c = System.in.read()) != -1 && c != '\n' && c != '\r') {
-            sb.append((char) c);
-        }
-        return sb.toString();
+    public static void main(String[] args) {
+        questao2();
     }
 }
