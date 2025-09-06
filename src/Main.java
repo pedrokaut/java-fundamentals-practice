d83693eb27f8afd602dab4cae40ff436e7eac933
pedrokaut <lexus04111@gmail.com>
2025-09-06 17:21:02 -0300

    Adição de novos codigos da lista!

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
         System.out.println("Digite a qtd máxima: ");
         qmax = input.nextFloat();
 
-        etqmedio = (qmax + qmin) / 2; // fórmula corrigida
+        etqmedio = (qmax + qmin) / 2;
         System.out.println("O estoque médio é: " + etqmedio);
     }
 
@@ -24,7 +24,7 @@ public class Main {
         System.out.println("Qual o valor em dólares que deseja converter?");
         valorDolar = input.nextFloat();
 
-        valorReal = valorDolar * cotacao; // fórmula corrigida
+        valorReal = valorDolar * cotacao;
         System.out.println("O valor em reais é: " + valorReal);
     }
     public static void questao3() {
@@ -59,8 +59,85 @@ public class Main {
         input.close();
 
     }
+    public static void questao4() {
+        Scanner input = new Scanner(System.in);
 
+        int A, B, C, D;
+
+
+        System.out.print("Digite o valor de A: ");
+        A = input.nextInt();
+        System.out.print("Digite o valor de B: ");
+        B = input.nextInt();
+        System.out.print("Digite o valor de C: ");
+        C = input.nextInt();
+        System.out.print("Digite o valor de D: ");
+        D = input.nextInt();
+
+        System.out.println("\n/// SOMAS ///");
+        System.out.println("A + B = " + (A + B));
+        System.out.println("A + C = " + (A + C));
+        System.out.println("A + D = " + (A + D));
+        System.out.println("B + C = " + (B + C));
+        System.out.println("B + D = " + (B + D));
+        System.out.println("C + D = " + (C + D));
+
+        System.out.println("\n/// MULTIPLICAÇÕES ///");
+        System.out.println("A * B = " + (A * B));
+        System.out.println("A * C = " + (A * C));
+        System.out.println("A * D = " + (A * D));
+        System.out.println("B * C = " + (B * C));
+        System.out.println("B * D = " + (B * D));
+        System.out.println("C * D = " + (C * D));
+
+        input.close();
+
+    }
+    public static void questao5() {
+        Scanner input = new Scanner(System.in);
+        float distancia,tempo,velocidade,litros;
+        System.out.print("digite o tempo gasto");
+        tempo = input.nextFloat();
+        System.out.print("digite a velocidade media");
+        velocidade = input.nextFloat();
+        distancia = tempo * velocidade;
+        litros = distancia / 12;
+        System.out.println("A velocidade media:" +velocidade);
+        System.out.println("O tempo gasto foi "+ tempo);
+        System.out.println("A distancia percorrida foi "+distancia);
+        System.out.println("A quantidade de litros foi "+litros);
+        input.close();
+    }
+    public static void questao6() {
+
+        Scanner input = new Scanner(System.in);
+        float celsius, fahrenheit;
+
+        System.out.print("digite a temperatura em graus Celsius: ");
+        celsius = input.nextFloat();
+
+        fahrenheit = (9 * celsius + 160) / 5;
+
+        System.out.println("A temperatura em Fahrenheit e: " + fahrenheit);
+
+        input.close();
+    }
+    public static void questao7() {
+        Scanner input = new Scanner(System.in);
+
+        float fahrenheit, celsius;
+
+        System.out.print("digite a temperatura em graus Fahrenheit: ");
+        fahrenheit = input.nextFloat();
+
+        celsius = (fahrenheit - 32) * 5 / 9;
+
+        System.out.println("A temperatura em Celsius e: " + celsius);
+
+        input.close();
+    }
     public static void main(String[] args) {
-        questao3();
+
+        questao6();
     }
 }
